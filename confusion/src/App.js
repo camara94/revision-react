c2bdceeadb29dd63a0fed63c95d18ed96886d9f4
camara94 <ldamaro98@gmail.com>
2020-03-23 05:42:09 +0100

    Components Partie II

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -2,23 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import DISH from './models/dish';
 
-
-const nav = (
-  <div>
-    <Navbar dark color="primary">
-      <div className="container">
-        <NavbarBrand href="/">Ristorant Con Fusion Camara</NavbarBrand>
-      </div>
-    </Navbar>
-    <Menu />
-  </div>
-);
 
 class App extends Component {
- render() {
-  return (nav);
- }
+
+  constructor( props ) {
+    super(props);
+    this.state = {
+      dishes: DISH
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorant Con Fusion Camara</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
